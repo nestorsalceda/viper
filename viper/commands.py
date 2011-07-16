d8fae0536d230dbcdd44bcc043e75812f54f1913
Néstor Salceda <nestor.salceda@gmail.com>
2011-07-16 19:31:48 +0200

    Implemented the command selection with the :action parameter.

--- viper/commands.py
+++ viper/commands.py
@@ -1,9 +1,23 @@
 # -*- coding: utf-8 -*-
 
-class CommandFactory(object):
-    def command_for(self, action):
-        return Command()
-
 class Command(object):
     def execute(self, **kwargs):
         raise NotImplementedError()
+
+class SubmitCommand(Command):
+    def execute(self, **kwargs):
+        raise NotImplementedError()
+
+class FileUploadCommand(Command):
+    def execute(self, **kwargs):
+        raise NotImplementedError()
+
+class CommandFactory(object):
+    _COMMANDS = {
+        'submit': SubmitCommand(),
+        'file_upload': FileUploadCommand()
+    }
+
+    def command_for(self, action):
+        return self._COMMANDS.get(action, Command())
+
